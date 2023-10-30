3aa55174d98c2e75cea1cd3fc961156f4fd68b17
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-10-30 14:39:32 +0100

    proyectos hechos en clase

---
+++ 01-trimestre/unidad-03/pruebaconexionbd/src/PruebaConexionBD.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PruebaConexionBD {
+    public static void main(String[] args)
+            throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/InstitutoFP";
+            String user = "postgres";
+            String password = "A123456a";
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión establecida.");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("No se pudo cargar controlador.");
+        } catch (SQLException ex) {
+            System.out.println("Error al conectar a la BD.");
+        } finally {
+            if (conn != null) conn.close();
+        }
+    }
+}
---
+++ 01-trimestre/unidad-03/statements/src/ConsultaBD1.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class ConsultaBD1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/InstitutoFP";
+        String user = "postgres";
+        String password = "A123456a";
+        Connection conn = DriverManager.getConnection(url, user, password);
+        Statement statement = conn.createStatement();
+        String SQLsentence = "SELECT * FROM \"Asignaturas\" ORDER BY codigo";
+        ResultSet rs = statement.executeQuery(SQLsentence);
+        System.out.println("Nombre"+"\t"+"Código");
+        System.out.println("---------------------------------");
+        while (rs.next()){
+            System.out.println(rs.getString(1) + "\t" + rs.getString(2));
+        }
+        rs.close();
+        conn.close();
+    }
+}
