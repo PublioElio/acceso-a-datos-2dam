a670b5f663eb1946c4b9b1f3ac661ae16c66ac82
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-20 14:18:27 +0100

    actualización actividad hibernate

--- 01-trimestre/unidad-03/Actividades/act_3_3/Act3_2.sql
+++ 01-trimestre/unidad-03/Actividades/act_3_3/Act3_2.sql
@@ -1,25 +1,25 @@
-DROP TABLE IF EXISTS departamentos;
-DROP TABLE IF EXISTS empleados;
+-- Database: InstitutoFP
 
-CREATE TABLE departamentos
-	(depno INTEGER PRIMARY KEY,
-	nombre VARCHAR(14),
-	ubicacion VARCHAR(13));
+-- DROP DATABASE IF EXISTS "InstitutoFP";
+CREATE TABLE  departamentos
+(depno INTEGER PRIMARY KEY,
+nombre VARCHAR(14),
+ubicacion VARCHAR(13));
 
 INSERT INTO departamentos VALUES (10, 'Contabilidad', 'Madrid');
 INSERT INTO departamentos VALUES (20, 'Marketing', 'Barcelona');
-INSERT INTO departamentos VALUES (10, 'Ventas', 'Alicante');
-INSERT INTO departamentos VALUES (10, 'Logistica', 'Valencia');
+INSERT INTO departamentos VALUES (30, 'Ventas', 'Alicante');
+INSERT INTO departamentos VALUES (40, 'Logística', 'Valencia');
 
-CREATE TABLE empleados
-	(empno INTEGER PRIMARY KEY,
-	nombre VARCHAR(10),
-	puesto VARCHAR(15),
-	depno INTEGER,
-		CONSTRAINT FK_EMPLOYEE_DPT FOREIGN KEY
-		(depno) REFERENCES departamentos(deptno) );
 
-INSERT INTO empleados VALUES (7369, 'Garcia', 'Dependiente', 20);
+CREATE TABLE  empleados
+(empno INTEGER PRIMARY KEY ,
+ nombre VARCHAR(10),
+ puesto VARCHAR(15),
+ depno INTEGER,
+CONSTRAINT FK_EMPLOYEE_DPT FOREIGN KEY(depno) REFERENCES departamentos(depno));
+
+INSERT INTO empleados VALUES (7369, 'García', 'Dependiente', 20);
 INSERT INTO empleados VALUES (7499, 'López', 'Vendedor', 30);
 INSERT INTO empleados VALUES (7521, 'Pérez', 'Vendedor', 30);
 INSERT INTO empleados VALUES (7566, 'Gómez', 'Responsable', 20);
@@ -28,13 +28,8 @@ INSERT INTO empleados VALUES (7698, 'Martínez', 'Responsable', 30);
 INSERT INTO empleados VALUES (7782, 'Sánchez', 'Responsable', 10);
 INSERT INTO empleados VALUES (7788, 'Jiménez', 'Analista', 20);
 INSERT INTO empleados VALUES (7839, 'Peláez', 'Presidente', 10);
-
---
-INSERT INTO empleados VALUES (7782, 'Sánchez', 'Responsable', 10);
-INSERT INTO empleados VALUES (7782, 'Sánchez', 'Responsable', 10);
-INSERT INTO empleados VALUES (7782, 'Sánchez', 'Responsable', 10);
-INSERT INTO empleados VALUES (7782, 'Sánchez', 'Responsable', 10);
-INSERT INTO empleados VALUES (7782, 'Sánchez', 'Responsable', 10);
-
-
-
+INSERT INTO empleados VALUES (7844, 'Álvarez', 'Vendedor', 30);
+INSERT INTO empleados VALUES (7876, 'Rodríguez', 'Dependiente', 20);
+INSERT INTO empleados VALUES (7900, 'Ramírez', 'Dependiente', 30);
+INSERT INTO empleados VALUES (7902, 'González', 'Analista', 20);
+INSERT INTO empleados VALUES (7934, 'Hernández', 'Dependiente', 10);
