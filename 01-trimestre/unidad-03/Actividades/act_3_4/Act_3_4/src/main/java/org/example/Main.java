6637fe02de9edee2a334e0c0399203679dbfadfd
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-23 10:17:26 +0100

    avances tarea 3_4

---
+++ 01-trimestre/unidad-03/Actividades/act_3_4/Act_3_4/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+            String url = "jdbc:pos";
+        }
+    }
+}
