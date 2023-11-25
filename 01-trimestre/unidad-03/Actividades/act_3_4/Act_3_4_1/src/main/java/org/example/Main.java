5b48ffc54f9560e80534edaab1613acaf1066293
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-25 18:06:33 +0100

    ejercicio 3.4.1

---
+++ 01-trimestre/unidad-03/Actividades/act_3_4/Act_3_4_1/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/InstitutoFP";
+            String user = "postgres";
+            String pwd = "A123456a";
+            Connection con = DriverManager.getConnection(url, user, pwd);
+            Statement statement = con.createStatement();
+            String SQLsentence = "SELECT * FROM \"Asignaturas\" order by codigo;";
+            ResultSet rs = statement.executeQuery(SQLsentence);
+            System.out.println("Nombre" + "\t\t\t\t\t" + "Año" + "\t\t\t\t\t" + "Código");
+            System.out.println("----------------------------------------------------");
+            while(rs.next()){
+                System.out.println(rs.getString(1) + "\t" + rs.getString(2)
+                        + "\t" + rs.getString(3));
+            }
+            rs.close();
+            con.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
