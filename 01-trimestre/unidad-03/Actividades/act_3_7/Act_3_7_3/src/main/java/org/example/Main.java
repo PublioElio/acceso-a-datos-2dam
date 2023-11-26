f26eda07b1ecb61ec36cf9da218d0f53d87e03b9
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-26 16:35:26 +0100

    ejercicio 3.7.3 hecho

--- 01-trimestre/unidad-03/Actividades/act_3_7/Act_3_7_3/src/main/java/org/example/Main.java
+++ 01-trimestre/unidad-03/Actividades/act_3_7/Act_3_7_3/src/main/java/org/example/Main.java
@@ -1,19 +1,35 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String url = "jdbc:postgresql://localhost:5432/empleados";
+        String user = "postgres";
+        String pass = "A123456a";
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR. " + e.getMessage());
         }
+
+        try (Connection con = DriverManager.getConnection(url, user, pass)) {
+
+            String nombre = EntradaTeclado.pedirCadena("Buscar empleados por nombre: ");
+            CallableStatement statement =
+                    con.prepareCall("{call lista_emp_nombre('" + nombre + "')}");
+            ResultSet rs = statement.executeQuery();
+            System.out.println("Código nombre" + "\t\tPuesto");
+            System.out.println("---------------------------------");
+            while (rs.next()) {
+                System.out.printf("%6d %-12s %s\n", rs.getInt(1), rs.getString(2), rs.getString(3));
+
+            }
+            statement.close();
+        } catch (SQLException e) {
+            System.out.println("ERROR. " + e.getMessage());
+        }
+
     }
 }
