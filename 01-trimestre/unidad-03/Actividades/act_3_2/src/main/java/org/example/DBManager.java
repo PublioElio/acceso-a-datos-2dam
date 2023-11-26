ea170ba7c0e8d9054c7f79fea35f9561d0808238
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-26 22:15:59 +0100

    finalizado ejercicio 3.2

--- 01-trimestre/unidad-03/Actividades/act_3_2/src/main/java/org/example/DBManager.java
+++ 01-trimestre/unidad-03/Actividades/act_3_2/src/main/java/org/example/DBManager.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBManager {
     private String url;
@@ -112,6 +111,7 @@ public class DBManager {
 
     /**
      * Esta función borra una tabla existente en la base de datos, con todas sus dependencias
+     *
      * @param tableName la tabla que hay que borrar
      * @return true si se ha borrado adecuadamente
      */
@@ -124,10 +124,265 @@ public class DBManager {
             statement.executeUpdate(dropTableQuery);
             deleted = true;
         } catch (SQLException e) {
-            System.out.println("ERROR. No se ha borrado la tabla " + tableName
-                    + ": " + e.getMessage());
+            System.out.println("ERROR. No se ha creado la tabla: " + e.getMessage());
         }
         return deleted;
+    }
+
+    /**
+     * Este es un método para insertar departamentos con un Prepared Statement. Recibe un array bidimensional de Objetos,
+     * en cada posición del array, hay una fila con los datos a insertar. A partir de este método se puede crear
+     * cualquier método para insertar elementos en una tabla, sabiendo el nombre, número de elementos y tipos de cada
+     * columna (ver ejemplo de insertar empleados)
+     *
+     * @param newDept los nuevos departamentos que vamos a insertar
+     * @return la cantidad de filas insertadas (integer)
+     */
+    public int insertDept(Object[][] newDept) {
+        String insertQuery = "INSERT INTO departamentos (nombre, presupuesto, gastos) VALUES (?, ?, ?)";
+        int totalNewDept = 0;
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = con.prepareStatement(insertQuery);
+            for (Object[] row : newDept) {
+                preparedStatement.setString(1, row[0].toString());
+                preparedStatement.setDouble(2, (Double) row[1]);
+                preparedStatement.setDouble(3, (Double) row[2]);
+                preparedStatement.executeUpdate();
+                totalNewDept++;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error insertando departamentos: " + e.getMessage());
+        }
+        return totalNewDept;
+    }
+
+    /**
+     * El siguiente método introduce nuevos empleados en la tabla empleados, con sus campos correspondientes, hay que
+     * tener en cuenta que algunos de los campos de esta tabla pueden ser nulos, el método los maneja con sentencias
+     * if/else
+     *
+     * @param newEmp un array bidimensional de objetos que contiene los datos a insertar
+     * @return un entero con el total de empleados nuevos insertados
+     */
+    public int insertEmp(Object[][] newEmp) {
+        String insertQuery = "INSERT INTO empleados (nif, nombre, apellido1, apellido2, id_departamento) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        int totalNewEmp = 0;
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = con.prepareStatement(insertQuery);
+            for (Object[] row : newEmp) {
+                preparedStatement.setString(1, row[0].toString());
+                preparedStatement.setString(2, row[1].toString());
+                preparedStatement.setString(3, row[2].toString());
+                if (row[3] != null) {
+                    preparedStatement.setString(4, row[3].toString());
+                } else {
+                    preparedStatement.setNull(4, java.sql.Types.VARCHAR); // este campo puede ser null
+                }
+                if (row[4] != null) {
+                    preparedStatement.setInt(5, (Integer) row[4]);
+                } else {
+                    preparedStatement.setNull(4, Types.INTEGER); // este campo puede ser null
+                }
+                preparedStatement.executeUpdate();
+                totalNewEmp++;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error insertando empleados: " + e.getMessage());
+        }
+        return totalNewEmp;
+    }
+
+    /**
+     * Este método hace una consulta de una sola columna de una tabla de la base de datos
+     *
+     * @param col   columna que vamos a listar
+     * @param table tabla que vamos a consultar
+     * @return una lista de String con el resultado de la consulta
+     */
+    public List<String> singleColumQuery(String col, String table) {
+        List<String> list = new ArrayList<>();
+        String query = "SELECT " + col + " FROM " + table + ";";
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String result = resultSet.getString(col);
+                list.add(result);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al listar la columna " + col
+                    + " de la tabla " + table + ": " + e.getMessage());
+        }
+        return list;
+    }
+
+    /**
+     * Este método te devuelve una relación de resultados distintos de una columna
+     *
+     * @param col   la columna de la que quieres obtener resultados distintos
+     * @param table la tabla donde vas a hacer la consulta
+     * @return un listado de String
+     */
+    public List<String> singleColumDistinct(String col, String table) {
+        List<String> list = new ArrayList<>();
+        String query = "SELECT DISTINCT " + col + " FROM " + table
+                + " WHERE " + col + " IS NOT NULL;";
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String result = resultSet.getString(col);
+                list.add(result);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+        return list;
+    }
+
+    /**
+     * Este método muestra por pantalla los dos departamentos con menor gasto
+     */
+    public void deptMenorGasto() {
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+            rs = query.executeQuery("SELECT nombre, gastos FROM departamentos ORDER BY gastos LIMIT 2;");
+            System.out.println("Los dos departamentos con menor gasto son:");
+            while (rs.next()) {
+                System.out.println(rs.getString("nombre") + ", gasto: " + rs.getDouble("gastos"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+
+    }
+
+    /**
+     * Este método muestra aquellos departamentos con un presupuesto mayor de 150000
+     */
+    public void deptPrepMayor() {
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+            rs = query.executeQuery("SELECT nombre, presupuesto FROM departamentos WHERE presupuesto >= 150000;");
+            System.out.println("Los departamentos con un presupuesto mayor a 150.000 son:");
+            while (rs.next()) {
+                System.out.println(rs.getString("nombre") + ", presupuesto: "
+                        + rs.getDouble("presupuesto"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Este método devuelve los empleados con los datos de sus departamentos
+     */
+    public void empDept() {
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+            rs = query.executeQuery("SELECT empleados.nombre AS empNombre, empleados.apellido1 AS empApellido1," +
+                    " empleados.apellido2 AS empApellido2, departamentos.nombre AS deptNombre " +
+                    "FROM empleados JOIN departamentos ON empleados.id_departamento = departamentos.id;");
+            System.out.println("Los empleados con los datos de sus departamentos son:");
+            while (rs.next()) {
+                System.out.println(rs.getString("empNombre") + " "
+                        + rs.getString("empApellido1")
+                        + " " + rs.getString("empApellido2")
+                        + ", departamento: " + rs.getString("deptNombre"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Este método lista los empleados y sus departamentos, ordenado alfabéticamente por el nombre del departamento y
+     * por los apellidos y el nombre de los empleados.
+     */
+    public void listarEmpDep() {
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+
+            rs = query.executeQuery("SELECT empleados.nombre AS empNombre, empleados.apellido1 AS empApellido1, " +
+                    "empleados.apellido2 AS empApellido2, departamentos.nombre AS deptNombre FROM empleados " +
+                    "JOIN departamentos ON empleados.id_departamento = departamentos.id " +
+                    "ORDER BY departamentos.nombre, empleados.apellido1, empleados.apellido2;");
+            System.out.println("Listado de empleados y los departamentos donde trabajan, " +
+                    "ordenados:");
+            while (rs.next()) {
+                System.out.println(rs.getString("empNombre") + " " + rs.getString("empApellido1")
+                        + " " + rs.getString("empApellido2") + ", departamento: "
+                        + rs.getString("deptNombre"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Este método devuelve la ID y el nombre de los departamentos que tienen empleados
+     */
+    public void depIdConEmpleados() {
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+
+            rs = query.executeQuery("SELECT id, nombre FROM departamentos " +
+                    "WHERE id NOT IN (SELECT id_departamento FROM empleados);");
+            System.out.println("Los departamentos que tienen empleados son los siguientes:");
+            while (rs.next()) {
+                System.out.println(rs.getString("nombre") + " " + rs.getInt("id"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Este método obtiene los departamentos donde trabaja un empleado, buscando por su NIF
+     */
+    public void buscarDeptPorNifEmp(){
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+            rs = query.executeQuery("SELECT nombre FROM departamentos WHERE id = " +
+                    "(SELECT id_departamento FROM empleados WHERE nif = '38382980M');");
+            System.out.println("Departamento/s donde trabaja el empleado con el NIF 38382980M: " );
+            while(rs.next()){
+                System.out.println(rs.getString("nombre"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Este método devuelve la suma total del presupuesto de todos los departamentos
+     */
+    public void sumarPresupuesto(){
+        ResultSet rs;
+        try {
+            Statement query = con.createStatement();
+            rs = query.executeQuery("SELECT SUM(presupuesto) AS sumaPresupuesto FROM departamentos;");
+            System.out.println("El presupuesto total de todos los departamentos es:");
+            while(rs.next()){
+                System.out.println(rs.getDouble("sumaPresupuesto"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta: " + e.getMessage());
+        }
     }
 
     public void setUrl(String url) {
--- 01-trimestre/unidad-03/Actividades/act_3_2/src/main/java/org/example/Main.java
+++ 01-trimestre/unidad-03/Actividades/act_3_2/src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
         //System.out.println("Borrar base de datos empleados: " + db.deleteDatabase("empleados"));
 
         // Crear las tablas:
+        /*
         String tablaDepartamentos = "CREATE TABLE departamentos ("
                 + "id SERIAL PRIMARY KEY,"
                 + "nombre VARCHAR(100) NOT NULL,"
@@ -30,9 +31,82 @@ public class Main {
                 + ")";
         System.out.println("Se ha creado la tabla 'departamentos': " + db.createTable(tablaDepartamentos));
         System.out.println("Se ha creado la tabla 'empleados': " + db.createTable(tablaEmpleados));
+        */
 
-        // Este línea de abajo es para probar que, efectivamente, funciona el método para borrar tablas
+        // Estas líneas de abajo es para probar que funciona el método para borrar tablas
         //System.out.println("Se ha borrado la tabla 'departamentos': " + db.deleteTable("departamentos"));
+        //System.out.println("Se ha borrado la tabla 'empleados': " + db.deleteTable("empleados"));
+
+        // Insertar elementos en las tablas **ATENCIÓN**: los elementos tipo double son declarados como tales
+        // para evitar problemas en la conversión de tipos en el PreparedStatement
+        /*
+        Object[][] nuevosDept = {
+                {"Desarrollo", 120000.0, 6000.0},
+                {"Sistemas", 150000.0, 21000.0},
+                {"Recursos Humanos", 280000.0, 25000.0},
+                {"Contabilidad", 110000.0, 3000.0},
+                {"I+D", 375000.0, 380000.0},
+                {"Proyectos", 0.0, 0.0},
+                {"Publicidad", 0.0, 1000.0}
+        };
+        System.out.println("Total de departamentos insertados: " + db.insertDept(nuevosDept));
+        */
+
+        /*
+        Object[][] nuevosEmp = {
+                {"32481596F", "Aarón", "Rivero", "Gómez", 1},
+                {"Y5575632D", "Adela", "Salas", "Díaz", 2},
+                {"R6970642B", "Adolfo", "Rubio", "Flores", 3},
+                {"77705545E", "Adrián", "Suárez", null, 4},
+                {"17087203C", "Marcos", "Loyola", "Méndez", 5},
+                {"38382980M", "María", "Santana", "Moreno", 1},
+                {"80576669X", "Pilar", "Ruiz", null, 2},
+                {"71651431Z", "Pepe", "Ruiz", "Santana", 3},
+                {"56399183D", "Juan", "Gómez", "López", 2},
+                {"46384486H", "Diego", "Flores", "Salas", 5},
+                {"67389283A", "Marta", "Herrera", "Gil", 1},
+                {"41234836R", "Irene", "Salas", "Flores", null},
+                {"82635162B", "Juan Antonio", "Sáez", "Guerrero", null}
+        };
+        System.out.println("Total de empleados insertados: " + db.insertEmp(nuevosEmp));
+        */
+
+        // 1 - Lista el primer apellido de todos los empleados.
+        System.out.println("Primer apellido de todos los empleados:\n"
+                + db.singleColumQuery("apellido1", "empleados"));
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 2 - Lista el primer apellido de los empleados eliminando los apellidos que estén repetidos.
+        System.out.println("Primer apellido de todos los empleados, sin repetir:\n"
+                + db.singleColumDistinct("apellido1", "empleados"));
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 3 - Devuelve una lista con el nombre y el gasto, de los 2 departamentos que tienen menor gasto.
+        db.deptMenorGasto();
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 4 - Devuelve una lista con el nombre de los departamentos y el presupuesto, de aquellos que tienen un presupuesto mayor o igual a 150000 euros.
+        db.deptPrepMayor();
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 5 - Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno.
+        db.empDept();
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 6 - Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno. Ordena el resultado, en primer lugar por el nombre del departamento (en orden alfabético) y en segundo lugar por los apellidos y el nombre de los empleados.
+        db.listarEmpDep();
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 7 - Devuelve un listado con el identificador y el nombre del departamento, solamente de aquellos departamentos que tienen empleados.
+        db.depIdConEmpleados();
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 8 - Devuelve el nombre del departamento donde trabaja el empleado que tiene el nif 38382980M.
+        db.buscarDeptPorNifEmp();
+        System.out.println("----------------------------------------------------------------------------");
+
+        // 9 - Calcula la suma del presupuesto de todos los departamentos.
+        db.sumarPresupuesto();
 
         System.out.println("Conexión terminada: " + db.closeConnection());
     }
