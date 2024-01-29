09c19a4e7d5057f6c1d32ba11a24d745b27e82a3
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-29 23:08:27 +0100

    continuamos con tarea 4_5

--- 02-trimestre/unidad-04/Actividades/Act4_5/Act4_5/src/main/java/org/example/Main.java
+++ 02-trimestre/unidad-04/Actividades/Act4_5/Act4_5/src/main/java/org/example/Main.java
@@ -1,13 +1,34 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Hello world!
- *
+ * Este es el main del ejercicio
  */
 public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( );
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        try {
+            if (session != null) {
+                System.out.println("Se ha inciado la sesión");
+            } else {
+                System.out.println("Error abriendo la sesión");
+            }
+        } catch (Exception e) {
+            Logger.getLogger(SessionFactory.class.getName()).log(Level.SEVERE, "Error al manejar la sesión", e);
+        } finally {
+            assert session != null;
+            session.close();
+            sessionFactory.close();
+        }
+
     }
 }
---
+++ 02-trimestre/unidad-04/Actividades/Act4_5/Act4_5/src/main/java/org/example/entities/User.java
@@ -0,0 +1,63 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users", schema = "public", catalog = "ExamenDiciembre")
+public class User {
+
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Id
+        @Column(name = "idUsuario", nullable = false)
+        private long id;
+        @Basic
+        @Column(name = "Nombre", length = 48)
+        private String name;
+        @Basic
+        @Column(name = "fechaCumpleanyos")
+        private Date birthDate;
+
+        public User(){
+        }
+        public long getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User that = (User) o;
+
+            if (id != that.id) return false;
+            return Objects.equals(name, that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = (int) id;
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+
+
+
+
+
+}
