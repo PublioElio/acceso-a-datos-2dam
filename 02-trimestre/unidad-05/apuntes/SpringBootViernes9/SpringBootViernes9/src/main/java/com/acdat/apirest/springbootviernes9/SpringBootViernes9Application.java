887e6306d10a5ed2b3cdaea40253a5616ef5f363
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-02-09 14:17:13 +0100

    primer proyecto Spring

---
+++ 02-trimestre/unidad-05/apuntes/SpringBootViernes9/SpringBootViernes9/src/main/java/com/acdat/apirest/springbootviernes9/SpringBootViernes9Application.java
@@ -0,0 +1,13 @@
+package com.acdat.apirest.springbootviernes9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootViernes9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootViernes9Application.class, args);
+	}
+
+}
