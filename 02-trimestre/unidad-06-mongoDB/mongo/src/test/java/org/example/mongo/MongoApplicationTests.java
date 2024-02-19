362226e2b133e98a8068f123855db1ba4d4117ae
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-02-19 14:34:00 +0100

    proyecto mongo

---
+++ 02-trimestre/unidad-06-mongoDB/mongo/src/test/java/org/example/mongo/MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
