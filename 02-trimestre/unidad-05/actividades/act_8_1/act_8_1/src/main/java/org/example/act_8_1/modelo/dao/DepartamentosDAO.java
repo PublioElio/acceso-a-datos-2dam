ced3ced7f5bf30ce0fb3c0f7978ffa1b1bf0058c
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-02-23 11:08:54 +0100

    proyecto 8.1

--- 02-trimestre/unidad-05/actividades/act_8_1/act_8_1/src/main/java/org/example/act_8_1/modelo/dao/DepartamentosDAO.java
+++ 02-trimestre/unidad-05/actividades/act_8_1/act_8_1/src/main/java/org/example/act_8_1/modelo/dao/DepartamentosDAO.java
@@ -1,12 +1,14 @@
 package org.example.act_8_1.modelo.dao;
 
+import org.example.act_8_1.modelo.entidades.DepartamentosEntity;
 import org.example.act_8_1.modelo.entidades.EmpleadosEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartamentosDAO extends CrudRepository<EmpleadosEntity, Integer> {
+public interface DepartamentosDAO extends CrudRepository<DepartamentosEntity, Integer> {
 
+    DepartamentosEntity findByNombre(String nombre);
 
-
+    
 }
